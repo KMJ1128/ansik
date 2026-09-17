368bc632b084ebd001594616a9db3b74b048e0ee
KMJ1128 <121225661+KMJ1128@users.noreply.github.com>
2026-09-17 17:27:49 +0900

    fix: import dp for navigation bar elevation

--- app/src/main/java/com/kmj/ansik/ui/AnsikApp.kt
+++ app/src/main/java/com/kmj/ansik/ui/AnsikApp.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.core.os.LocaleListCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
